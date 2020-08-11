9febfe59ef985a57311772eaad676f92709967a5
Marlom Klismann <MKlismann@users.noreply.github.com>
2020-08-11 20:54:07 -0300

    Inclusão de um mostrador de caracteres da forca + Saída do Loop após acerto

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,8 +4,11 @@ def jogar():
     print("******************************************************************")
 
     palavra_secreta = "guitarra"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_", "_"]
     enforcado = False
     acertou = False
+
+    print(f"\n{letras_acertadas}")
 
     # Roda o jogo enquanto o jogador não acertar a palavra, e não tiver esgotado todas as tentativas disponíves.
     while not enforcado and not acertou:
@@ -16,9 +19,15 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if letra.upper() == chute.upper():
-                print(f"Encontrei a letra {letra} na posição {index}")
+                letras_acertadas[index] = letra
 
             index = index + 1
+
+        print(f"\n{letras_acertadas}")
+
+        acertou = letras_acertadas.count('_') == 0
+        if not acertou:
+            print(f"Ainda falta acertar {letras_acertadas.count('_')} letras")
 
     print("\n******************************************************************")
     print("*                        Fim do jogo !!!                         *")
